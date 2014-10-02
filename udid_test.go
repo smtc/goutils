b2cd859b698020e41feaa3132fbd41e623f620fd
lobos <lobos841@gmail.com>
2014-10-02 22:48:48 +0800

    ObjectId

---
+++ udid_test.go
@@ -0,0 +1,18 @@
+package goutils
+
+import (
+	"testing"
+)
+
+func TestObjectID(t *testing.T) {
+	println(ObjectID())
+	println(ObjectID())
+	println(ObjectID())
+	println(ObjectID())
+	println(ObjectID())
+	println(ObjectID())
+	println(ObjectID())
+	println(ObjectID())
+	println(ObjectID())
+	println(ObjectID())
+}
